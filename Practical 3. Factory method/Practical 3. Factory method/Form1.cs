2d1701ceb750c2a9f51722410eb89ecda2695253
Danilo2077 <danilogerasymovsky1@gmail.com>
2024-12-01 21:58:10 +0200

    Add files via upload

---
+++ Practical 3. Factory method/Practical 3. Factory method/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practical_3.Factory_method
+{
+    public partial class Form1 : Form
+    {
+        private ShapeFactory _currentFactory;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Метод для зміни фабрики
+        private void ChangeFactory(ShapeFactory factory)
+        {
+            _currentFactory = factory;
+        }
+
+        // Метод для відображення фігури
+        private void ShowShape()
+        {
+            if (_currentFactory != null)
+            {
+                DrawShape shape = _currentFactory.CreateShape();
+                using (Graphics graphics = this.CreateGraphics())
+                {
+                    shape.Clear(graphics, this.BackColor, this.ClientRectangle);
+                    shape.Draw(graphics);
+                }
+            }
+        }
+
+        private void ButtonCube_Click(object sender, EventArgs e)
+        {
+            ChangeFactory(new CubeFactory());  // Встановлення фабрики для Куба
+            ShowShape();
+        }
+
+        private void ButtonCylinder_Click(object sender, EventArgs e)
+        {
+            ChangeFactory(new CylinderFactory());  // Встановлення фабрики для Циліндра
+            ShowShape();
+        }
+
+        private void ButtonPyramid_Click(object sender, EventArgs e)
+        {
+            ChangeFactory(new PyramidFactory());  // Встановлення фабрики для Піраміди
+            ShowShape();
+        }
+    }
+}
---
+++ Practical 3. Factory method/Practical 3. Factory method/ShapeFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_3.Factory_method
+{
+    public abstract class ShapeFactory
+    {
+        public abstract DrawShape CreateShape();
+    }
+
+    public class CubeFactory : ShapeFactory
+    {
+        public override DrawShape CreateShape()
+        {
+            return new Cube();
+        }
+    }
+
+    public class CylinderFactory : ShapeFactory
+    {
+        public override DrawShape CreateShape()
+        {
+            return new Cylinder();
+        }
+    }
+
+    public class PyramidFactory : ShapeFactory
+    {
+        public override DrawShape CreateShape()
+        {
+            return new Pyramid();
+        }
+    }
+}
